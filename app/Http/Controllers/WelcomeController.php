f2cbe4e8829e899f491cd4bee01ebb66bc46db16
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-23 18:35:21 +0200

    added method in WelcomeController  to get all the trains in welcome view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Train;
 class WelcomeController extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::all();
+        return view('welcome',compact('trains'));
+    } 
 }
