2c8534c1256288792c93b37a812e06a0cb335a8d
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-23 15:59:59 +0200

    added example route to web.php file in routes folder

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GuestPageController::class, 'getMovies'])->name('homepage');
+
+/* example route*/
