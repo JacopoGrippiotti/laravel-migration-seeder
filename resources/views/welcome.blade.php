d2031a0f5d469601c92924db8f581a8e47d2c038
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-23 16:00:31 +0200

    added example structure for welcome.blade.php file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,29 @@
 
 @section('title', 'Welcome')
 
+
 @section('main-content')
-    <h1>
-        Welcome
+   
+    <h1 class="text-center">
+        Homepage
     </h1>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h3>
-                    ciaone
-                </h3>
+      <div class="row justify-content-center">
+        
+        @foreach ($trains as $train)
+            {{-- @dump($book) --}}
+            <div class="card" style="width: 14rem;">
+                <h4>{{}}</h4>
+                <div class="card-body">
+                   <h5 class="card-title">{{}}</h5>
+                   <p class="card-text">{{}}</p>
+                   <p class="card-text">{{}}</p>
+                   <p class="card-text">{{}}</p>                  
+                </div>
             </div>
-        </div>
+           
+        @endforeach
+      </div>   
     </div>
 @endsection
 
